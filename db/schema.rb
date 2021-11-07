3387c8da97fbd829e7ed5081a33984703753f9de
YoungBinKo <youngbinkoko@gmail.com>
2021-11-07 14:13:50 +0100

    solved Issue 4

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_07_112618) do
-
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "text"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2021_11_07_124044) do
 
   create_table "authors", force: :cascade do |t|
-    t.string "title"
-    t.text "text"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "homepage"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
