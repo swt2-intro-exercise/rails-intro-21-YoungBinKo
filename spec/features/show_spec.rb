d07a310e8a2c489de3ef2fc877ad7aaaf4124a9d
YoungBinKo <youngbinkoko@gmail.com>
2021-11-19 12:21:44 +0100

    complete issue #6

--- spec/features/show_spec.rb
+++ spec/features/show_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 describe "New show page", type: :feature do
-    it "should exist at 'show_path' and render withour error" do
-      visit show_path
-    end
-    it "should have text inputs for an author's first name, last name, and homepage" do
-      visit show_path
+    it "should display author's details, which are author's first name, last name, and homepage" do
       FactoryBot.create :author
+      visit new_author_path 
+      page.fill_in 'author[last_name]', with: 'author.first_name'
+      page.fill_in 'author[first_name]', with: 'author.last_name'
+      page.fill_in 'author[homepage]', with: 'author.homepage'
+      find('input[type="submit"]').click
+      
       # these are the standard names given to inputs by the Rails form builder
       expect(page).to have_text('author.first_name')
       expect(page).to have_text('author.last_name')
