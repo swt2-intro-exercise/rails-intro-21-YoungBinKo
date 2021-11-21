68fd39506bb5c62f46ea03d0d501e151a6006781
YoungBinKo <youngbinkoko@gmail.com>
2021-11-21 21:02:20 +0100

    chekcing issue #14

--- db/migrate/20211121175143_create_papers.rb
+++ db/migrate/20211121175143_create_papers.rb
@@ -4,7 +4,7 @@ class CreatePapers < ActiveRecord::Migration[6.0]
       t.string :title
       t.string :venue
       t.integer :year
-      t.list :authors
+      #t.string :author
 
       t.timestamps
     end
