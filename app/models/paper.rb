79277a029494094afba8b3ea3eb9f1751c64f264
YoungBinKo <youngbinkoko@gmail.com>
2021-11-21 19:22:10 +0100

    complete #12

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,2 +1,4 @@
 class Paper < ApplicationRecord
+    validates :title, :venue, presence: true, length: {minimum: 1}
+    validates :year, presence:true, numericality: { only_integer: true}
 end
--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
   it "should fail validation, when a paper was created without title, venue and year" do
-    paper = Paper.new(title: 'test', venue: 'place', year:'2020')
+    paper = Paper.new(venue: 'place', year:'2020')
     expect(paper).to_not be_valid
   end
 end
