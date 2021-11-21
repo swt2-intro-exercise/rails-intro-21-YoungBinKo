262078f4a26d4897a2f4bca48532e47ddd79c98f
YoungBinKo <youngbinkoko@gmail.com>
2021-11-21 19:40:13 +0100

    complete #13

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,4 +1,5 @@
 class Paper < ApplicationRecord
     validates :title, :venue, presence: true, length: {minimum: 1}
     validates :year, presence:true, numericality: { only_integer: true}
+    has_and_belongs_to_many :authors
 end
