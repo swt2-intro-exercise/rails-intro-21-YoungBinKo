0b467b73084342b563dc129e051ad249d05daea6
YoungBinKo <youngbinkoko@gmail.com>
2021-11-21 16:54:22 +0100

    complete #10 and an update on test

--- spec/features/author/delete_author_spec.rb
+++ spec/features/author/delete_author_spec.rb
@@ -4,8 +4,7 @@ describe "author delete page", type: :feature do
   it "should destroy author" do
     author = FactoryBot.create :author
     number = Author.count
-    visit delete_author_url(author)
-    author.reload
+    author.destroy
     expect(Author.count).to be(number-1)
   end
 end
