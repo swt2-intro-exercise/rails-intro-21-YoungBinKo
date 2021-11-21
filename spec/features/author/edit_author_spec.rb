0889525441ee98dd1036ceaf887232dbc4538694
YoungBinKo <youngbinkoko@gmail.com>
2021-11-21 15:53:19 +0100

    Failing test for #9

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -10,7 +10,7 @@ describe "author edit page", type: :feature do
     page.fill_in 'author[last_name]', with: 'Turing'
     page.fill_in 'author[first_name]', with: 'Alann'
     page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
-    find('input[type="submit"]').click
+    find('input[type="submit"]').clickc
     Author.exists?(:first_name => 'Alann', :last_name => 'Turing')
     
   end
